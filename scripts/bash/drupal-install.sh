a92a598858f633dbdb35f5481984740df74ca499
Gerner Péter <gerner@argopecten.hu>
2023-10-06 14:25:17 +0200

    handle drupal home directory

--- scripts/bash/drupal-install.sh
+++ scripts/bash/drupal-install.sh
@@ -9,8 +9,8 @@ usage() { echo "$0 usage:" && grep " .)\ #" $0; exit 0; }
 [ $# -eq 0 ] && usage
 
 # Install LAMP components for Aegir
-echo "DP | ------------------------------------------------------------------"
-echo "DP | A) Site parameter ..."
+echo "DI | ------------------------------------------------------------------"
+echo "DI | A) Site parameter ..."
 
 # u: site URL: drupal.local
 while getopts 'u:' OPTION; do
@@ -26,11 +26,26 @@ while getopts 'u:' OPTION; do
   esac
 done
 
-# Install LAMP components for Aegir
-echo "DP | ------------------------------------------------------------------"
-echo "DP | B) Installing Drupal with Composer ..."
+echo "DI | --------------------------------------------------------------------"
+echo "DI | B) Prepare drupal folder ..."
+
+# get Drupal project directory
+DRUPAL_HOME="/var/drupal"
+
+if [ ! -d "${DRUPAL_HOME}" ] ; then
+  # create missing /var/drupal folder
+  sudo mkdir ${DRUPAL_HOME}
+  sudo chown `whoami` ${DRUPAL_HOME}
+else
+  # clean up previous site folder if any
+  [[ -f "${DRUPAL_HOME}/$SITE_URI" ]] && sudo rm -rf "${DRUPAL_HOME}/$SITE_URI"
+fi
+
+# Create project from composer.json
+echo "DI | ------------------------------------------------------------------"
+echo "DI | C) Installing Drupal with Composer ..."
 
 composer create-project argopecten/drupal-project \
          --no-interaction \
          --repository '{"type": "vcs","url":  "https://github.com/argopecten/drupal-project"}' \
-         $SITE_URI
+         "${DRUPAL_HOME}/$SITE_URI"
